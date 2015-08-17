b0ce2b3890057d56f20986f61861d53faadc6c64
mkidushim <mkidushim@gmail.com>
2015-08-16 18:49:42 -0700

    node2

--- node2/server.js
+++
@@ -1,6 +0,0 @@
-var port = 8080;
-var express = require('./config/express');
-var app = express();
-app.listen(port);
-module.exports = app;
-console.log('Server running at http://localhost:' + port);
